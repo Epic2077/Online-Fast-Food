90531aae0fa4bd7801ebc77c037f3742793cdba2
Ashkan <the.epic.2077@gmail.com>
2024-12-06 00:58:14 +0330

    feat(Cart): adding Cart to products
    Trying to add cart functionality to the project

--- src/Components/cards/FoodCard.tsx
+++ src/Components/cards/FoodCard.tsx
@@ -1,12 +1,35 @@
 import React, { useState } from "react";
 import { Product } from "../../Api/products";
 
+export const cartArray: { product: Product; quantity: number }[] = [];
 interface FoodCardProps {
   product: Product;
 }
 
 const FoodCard: React.FC<FoodCardProps> = ({ product }) => {
   const [count, setCount] = useState<number>(0);
+
+  const updateCart = (product: Product, quantity: number) => {
+    const index = cartArray.findIndex(
+      (item) => item.product.name === product.name
+    );
+
+    if (quantity > 0) {
+      if (index === -1) {
+        cartArray.push({ product, quantity });
+        console.log("Push ", cartArray);
+      } else {
+        cartArray[index].quantity = quantity;
+        console.log("quantity Change ", cartArray);
+      }
+    } else {
+      if (index !== -1) {
+        cartArray.splice(index, 1);
+        console.log("cart Remove ", cartArray);
+      }
+    }
+    console.log("Updated Cart:", cartArray);
+  };
   return (
     <div className="w-[26%] h-[400px] shadow-[0px_4px_40px_-16px_rgba(0,_0,_0,_0.8)] rounded-[10px] p-6">
       <img
@@ -22,15 +45,25 @@ const FoodCard: React.FC<FoodCardProps> = ({ product }) => {
         </p>
         <div className=" flex justify-between mr-5  border-black border-2 w-[110px] items-center rounded-[10px]">
           <div
-            className="ml-[-25px] p-[10px] w-[40px] flex justify-center items-center bg-gray-300 rounded-[10px]"
-            onClick={() => setCount(count - 1)}
+            className="ml-[-25px] p-[10px] w-[40px] flex justify-center items-center bg-gray-300 rounded-[10px] "
+            onClick={() => {
+              if (count > 0) {
+                setCount(count - 1);
+                updateCart(product, count);
+              } else {
+                return;
+              }
+            }}
           >
             -
           </div>
           <p>{count}</p>
           <div
             className="text-white bg-orange-500 p-[10px] w-[40px] flex justify-center items-center mr-[-25px] rounded-[10px]"
-            onClick={() => setCount(count + 1)}
+            onClick={() => {
+              setCount(count + 1);
+              updateCart(product, count);
+            }}
           >
             +
           </div>
